c8b98c11818d8ceeed6c905af9b7c309d2bd7fd3
Chase Ross <chaseross002@gmail.com>
2025-04-09 14:03:43 -0400

    Made the powerup spawn at a random place on screen

--- Assets/Scripts/Powerup Spawner.cs
+++ Assets/Scripts/Powerup Spawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PowerupSpawner : MonoBehaviour
 {
+    public GameObject powerUp;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class PowerupSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 pos = new Vector2 (Random.Range(0, Screen.width), Random.Range(0,Screen.height));
+
+        Vector2 screenPos = Camera.main.ScreenToWorldPoint(pos);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(powerUp, screenPos, Quaternion.identity);
+        }
     }
 }

b8c9a368b9c100b28623f5c1df5a47924ae9b15d
Chase Ross <chaseross002@gmail.com>
2025-04-09 13:44:13 -0400

    Limited player movement to screen space

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,51 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float pos;
     float speed = 10;
+
+    bool hasItem = false; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        turn();
+        Move();
     }
 
-    void turn()
+    void Move()
     {
+        Vector3 pos = transform.localPosition;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.eulerAngles = Vector3.forward * 0;
-            transform.localPosition += Vector3.up * Time.deltaTime * speed;
+
+            if (screenPos.y+30 < Screen.height)
+            {
+                pos = transform.localPosition + Vector3.up * Time.deltaTime * speed;
+                transform.localPosition = pos;
+            }
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.eulerAngles = Vector3.forward * 90;
-            transform.localPosition += Vector3.left * Time.deltaTime * speed;
+
+            if (screenPos.x-30 > 0)
+            {
+                pos = transform.localPosition + Vector3.left * Time.deltaTime * speed;
+                transform.localPosition = pos;
+            }
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.eulerAngles = Vector3.forward * -90;
-            transform.localPosition += Vector3.right * Time.deltaTime * speed;
 
+            if (screenPos.x + 30 < Screen.width)
+            {
+                pos = transform.localPosition + Vector3.right * Time.deltaTime * speed;
+                transform.localPosition = pos;
+            }
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.eulerAngles = Vector3.forward * 180;
-            transform.localPosition += Vector3.down * Time.deltaTime * speed;
 
+            if (screenPos.y - 30 > 0)
+            {
+                pos = transform.localPosition + Vector3.down * Time.deltaTime * speed;
+                transform.localPosition = pos;
+            }
         }
     }
 
-    void Move()
+    void Attack()
     {
         
     }
